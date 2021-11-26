7d6cd44b6dfe846553ecf891f5f9559805ffd53d
ichi-01 <ichikawa@rhems-japan.co.jp>
2021-11-26 14:27:40 +0900

    sh fix 4

--- src/scripts/create.sh
+++ src/scripts/create.sh
@@ -1,5 +1,7 @@
 TIME=$(date '+%Y-%m-%d-%H-%M-%S')
 
+echo "[before] ${API_TOKEN}"
+
 API_TOKEN=$(eval echo "\$${API_TOKEN}")
 ORGANIZATION=$(eval echo "\$${ORGANIZATION}")
 REPO=$(eval echo "\$${REPO}")
@@ -8,6 +10,8 @@ BRANCH=$(eval echo "\$${BRANCH}")
 STATUS=$(eval echo "\$${STATUS}")
 COLOR=$(eval echo "\$${COLOR}")
 TEXT=$(eval echo "\$${TEXT}")
+
+echo "[after] ${API_TOKEN}"
 
 curl -X POST -H "Content-Type: application/json" \
           https://badges.rhems-japan.com/api-update-badge \
