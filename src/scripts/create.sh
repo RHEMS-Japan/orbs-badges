e16fd2e3cffecef60242cd45fe00b7c1c118b035
ichi-01 <ichikawa@rhems-japan.co.jp>
2021-11-26 22:22:19 +0900

    test

--- src/scripts/create.sh
+++ src/scripts/create.sh
@@ -1,10 +1,15 @@
 TIME=$(date '+%Y-%m-%d-%H-%M-%S')
 
-echo "[before] ${API_TOKEN}"
+echo "[before]API_TOKEN ${API_TOKEN}"
+echo "[before]ORGANIZATION ${ORGANIZATION}"
+echo "[before]REPO ${REPO}"
+echo "[before]APP ${APP}"
+echo "[before]BRANCH ${BRANCH}"
+echo "[before]STATUS ${STATUS}"
+echo "[before]COLOR ${COLOR}"
+echo "[before]TEXT ${TEXT}"
 
 API_TOKEN=$(eval echo "\$$API_TOKEN")
-API_TOKEN2=$(eval echo "$API_TOKEN")
-
 ORGANIZATION=$(eval echo "\$${ORGANIZATION}")
 REPO=$(eval echo "\$${REPO}")
 APP=$(eval echo "\$${APP}")
@@ -13,8 +18,14 @@ STATUS=$(eval echo "\$${STATUS}")
 COLOR=$(eval echo "\$${COLOR}")
 TEXT=$(eval echo "\$${TEXT}")
 
-echo "[after] ${API_TOKEN}"
-echo "[after] ${API_TOKEN2}"
+echo "[after]API_TOKEN ${API_TOKEN}"
+echo "[after]ORGANIZATION ${ORGANIZATION}"
+echo "[after]REPO ${REPO}"
+echo "[after]APP ${APP}"
+echo "[after]BRANCH ${BRANCH}"
+echo "[after]STATUS ${STATUS}"
+echo "[after]COLOR ${COLOR}"
+echo "[after]TEXT ${TEXT}"
 
 curl -X POST -H "Content-Type: application/json" \
           https://badges.rhems-japan.com/api-update-badge \
