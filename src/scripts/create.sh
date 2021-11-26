37bd2aacd73d4cb1fe12536bc29de7ec6d4e936b
ichi-01 <ichikawa@rhems-japan.co.jp>
2021-11-26 13:01:22 +0900

    fix

--- src/scripts/create.sh
+++ src/scripts/create.sh
@@ -1,4 +1,4 @@
-TIME=`date '+%Y-%m-%d-%H-%M-%S'`
+TIME=$(date '+%Y-%m-%d-%H-%M-%S')
 curl -X POST -H "Content-Type: application/json" \
           https://badges.rhems-japan.com/api-update-badge \
            -d '{"api_token": "$API_TOKEN",
