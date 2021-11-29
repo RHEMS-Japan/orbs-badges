90b7d8280f652556a2c9bc1d0df655aa17e93083
RayMeibergen <rayman@rhems-japan.co.jp>
2021-11-30 03:32:37 +0900

    rayman did

--- src/scripts/create_badges.sh
+++ src/scripts/create_badges.sh
@@ -75,19 +75,6 @@ if [ ${HTTP_RESPONSE} -ne '200' ]; then
 fi
 }
 
-update_readme () {
-  echo "== run update_readme =="
-  if [ -n "${CIRCLE_BRANCH}" ]; then
-    sed -i -e "s#branch=.*\&cised=true.*#branch=${CIRCLE_BRANCH}\&cised=true\&update=$(date "+%Y%m%d-%H%M%S")\)#g" ${FILE_PATH}
-    git config --global user.email ${GIT_USER_EMAIL}
-    git config --global user.name "${GIT_USER_NAME}"
-    git add ${FILE_PATH}
-    echo "--- run git ---"
-    git commit -m "[skip ci] ${FILE_PATH} Update"
-    git push origin ${CIRCLE_BRANCH}
-  fi
-}
-
 ################## ---- main
 # check_debug
 if [ ! -n "${TEXT}" ]; then
@@ -95,5 +82,3 @@ if [ ! -n "${TEXT}" ]; then
 else
   ${STATUS} && post_to_badgeserver
 fi
-
-${UPDATE_README} && update_readme
