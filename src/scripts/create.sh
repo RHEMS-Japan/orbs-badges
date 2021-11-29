e251bb17a89ea4cbee75c3f05d2201cc2007c5e9
RayMeibergen <rayman@rhems-japan.co.jp>
2021-11-30 03:15:32 +0900

    rayman did

--- src/scripts/create.sh
+++ src/scripts/create.sh
@@ -80,7 +80,7 @@ update_readme () {
   if [ -n "${CIRCLE_BRANCH}" ]; then
     sed -i -e "s#branch=.*\&cised=true.*#branch=${CIRCLE_BRANCH}\&cised=true\&update=$(date "+%Y%m%d-%H%M%S")\)#g" ${FILE_PATH}
     git config --global user.email ${GIT_USER_EMAIL}
-    git config --global user.name ${GIT_USER_NAME}
+    git config --global user.name "${GIT_USER_NAME}"
     git add ${FILE_PATH}
     echo "--- run git ---"
     git commit -m "[skip ci] ${FILE_PATH} Update"
