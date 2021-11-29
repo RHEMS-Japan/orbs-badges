0da69c1c41eda97c3f82cff028a3cea8d918def0
ichi-01 <ichikawa@rhems-japan.co.jp>
2021-11-29 12:11:04 +0900

    test

--- src/scripts/create.sh
+++ src/scripts/create.sh
@@ -8,7 +8,7 @@ fi
 
 API_TOKEN=`eval echo "\$${TOKEN}"`
 
-echo "API_TOKEN ${TOKEN}"
+echo "API_TOKEN ${API_TOKEN}"
 echo "ORGANIZATION ${ORGANIZATION}"
 echo "REPO ${REPO}"
 echo "APP ${APP}"
@@ -18,33 +18,33 @@ echo "COLOR ${COLOR}"
 echo "TEXT ${TEXT}"
 echo $TIME
 
-curl -X POST -H "Content-Type: application/json" \
-https://badges.rhems-japan.com/api-update-badge \
--d "{\"api_token\": \"${API_TOKEN}\",
-\"organization\": \"${ORGANIZATION}\",
-\"repo\": \"${REPO}\",
-\"app\": \"${APP}\",
-\"branch\": \"${BRANCH}\",
-\"status\": \"${STATUS}\",
-$([ -n "${TEXT}" ] && \
-cat << EOF
-"txt": "${TEXT}",
-EOF)
-$([ -n "${COLOR}" ] && \
-cat << EOF
-"color": "${COLOR}",
-EOF)
-\"update\": \"${TIME}\"}"
-
-
 # curl -X POST -H "Content-Type: application/json" \
-#   https://badges.rhems-japan.com/api-update-badge \
-#   -d "{\"api_token\": \"${API_TOKEN}\",
-#                 \"organization\": \"${ORGANIZATION}\",
-#                 \"repo\": \"${REPO}\",
-#                 \"app\": \"${APP}\",
-#                 \"branch\": \"${BRANCH}\",
-#                 \"status\": \"${STATUS}\",
-#                 \"color\": \"${COLOR}\",
-#                 \"txt\": \"${TEXT}\",
-#                 \"update\": \"${TIME}\"}"
+# https://badges.rhems-japan.com/api-update-badge \
+# -d "{\"api_token\": \"${API_TOKEN}\",
+# \"organization\": \"${ORGANIZATION}\",
+# \"repo\": \"${REPO}\",
+# \"app\": \"${APP}\",
+# \"branch\": \"${BRANCH}\",
+# \"status\": \"${STATUS}\",
+# $([ -n "${TEXT}" ] && \
+# cat << EOF
+# "txt": "${TEXT}",
+# EOF)
+# $([ -n "${COLOR}" ] && \
+# cat << EOF
+# "color": "${COLOR}",
+# EOF)
+# \"update\": \"${TIME}\"}"
+
+
+curl -X POST -H "Content-Type: application/json" \
+  https://badges.rhems-japan.com/api-update-badge \
+  -d "{\"api_token\": \"${API_TOKEN}\",
+                \"organization\": \"${ORGANIZATION}\",
+                \"repo\": \"${REPO}\",
+                \"app\": \"${APP}\",
+                \"branch\": \"${BRANCH}\",
+                \"status\": \"${STATUS}\",
+                \"color\": \"${COLOR}\",
+                \"txt\": \"${TEXT}\",
+                \"update\": \"${TIME}\"}"
