53aeff2f1f40b9bc79dd7531eb2347e5ed29f5cf
ichi-01 <ichikawa@rhems-japan.co.jp>
2021-11-29 17:45:40 +0900

    200

--- src/scripts/create.sh
+++ src/scripts/create.sh
@@ -14,6 +14,26 @@ _repo=`eval echo "${REPO}"`
 _branch=`eval echo "${BRANCH}"`
 _txt=`eval echo "${TEXT}"`
 
+curl -X POST -H "Content-Type: application/json" \
+https://badges.rhems-japan.com/api-update-badge \
+-d "{\"api_token\": \"${_api_token}\",
+\"organization\": \"${_org}\",
+\"repo\": \"${_repo}\",
+\"app\": \"${APP}\",
+\"branch\": \"${_branch}\",
+\"status\": \"${STATUS}\",
+$([ -n "${_txt}" ] && \
+cat << EOF
+"txt": "${_txt}",
+EOF
+)
+$([ -n "${COLOR}" ] && \
+cat << EOF
+"color": "${COLOR}",
+EOF
+)
+\"update\": \"${_time}\"}"
+
 
 HTTP_RESPONSE=$(curl -o /dev/null --silent --write-out '%{http_code}\n' -X POST -H "Content-Type: application/json" \
 https://badges.rhems-japan.com/api-update-badge \
@@ -36,27 +56,9 @@ EOF
 \"update\": \"${TIME}\"}")
 echo "HTTP_RESPONSE=${HTTP_RESPONSE}"
 # Responses other than 200 end with an error.
-[ "${HTTP_RESPONSE}" != "200" ] && exit 1
+[ ${HTTP_RESPONSE} != 200 ] && exit 1
 
-# curl -X POST -H "Content-Type: application/json" \
-# https://badges.rhems-japan.com/api-update-badge \
-# -d "{\"api_token\": \"${_api_token}\",
-# \"organization\": \"${_org}\",
-# \"repo\": \"${_repo}\",
-# \"app\": \"${APP}\",
-# \"branch\": \"${_branch}\",
-# \"status\": \"${STATUS}\",
-# $([ -n "${_txt}" ] && \
-# cat << EOF
-# "txt": "${_txt}",
-# EOF
-# )
-# $([ -n "${COLOR}" ] && \
-# cat << EOF
-# "color": "${COLOR}",
-# EOF
-# )
-# \"update\": \"${_time}\"}"
+
 
 # curl -X POST -H "Content-Type: application/json" \
 #           https://badges.rhems-japan.com/api-update-badge \
