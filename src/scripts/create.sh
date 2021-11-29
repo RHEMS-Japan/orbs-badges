c31fc5c52fd68e220d7f392003070bda68dbcc59
ichi-01 <ichikawa@rhems-japan.co.jp>
2021-11-29 10:37:19 +0900

    sh

--- src/scripts/create.sh
+++ src/scripts/create.sh
@@ -6,44 +6,14 @@ else
   STATUS="false"
 fi
 
-echo "[before]API_TOKEN ${API_TOKEN}"
-echo "[before]ORGANIZATION ${ORGANIZATION}"
-echo "[before]REPO ${REPO}"
-echo "[before]APP ${APP}"
-echo "[before]BRANCH ${BRANCH}"
-echo "[before]STATUS ${STATUS}"
-echo "[before]COLOR ${COLOR}"
-echo "[before]TEXT ${TEXT}"
-
-API_TOKEN=$(eval echo "\$${API_TOKEN}")
-# ORGANIZATION=$(eval echo "\$${ORGANIZATION}")
-REPO=$(eval echo "${REPO}")
-# APP=$(eval echo "\$${APP}")
-BRANCH=$(eval echo "${BRANCH}")
-# STATUS=$(eval echo "\$$STATUS")
-# COLOR=$(eval echo "\$$COLOR")
-TEXT=$(eval echo "${TEXT}")
-
-echo "[after]API_TOKEN ${API_TOKEN}"
-echo "[after]ORGANIZATION ${ORGANIZATION}"
-echo "[after]REPO ${REPO}"
-echo "[after]APP ${APP}"
-echo "[after]BRANCH ${BRANCH}"
-echo "[after]STATUS ${STATUS}"
-echo "[after]COLOR ${COLOR}"
-echo "[after]TEXT ${TEXT}"
-
-# curl -X POST -H "Content-Type: application/json" \
-#   https://badges.rhems-japan.com/api-update-badge \
-#   -d "{\"api_token\": \"${API_TOKEN}\",
-#                 \"organization\": \"${ORGANIZATION}\",
-#                 \"repo\": \"${REPO}\",
-#                 \"app\": \"${APP}\",
-#                 \"branch\": \"${BRANCH}\",
-#                 \"status\": \"${STATUS}\",
-#                 \"color\": \"${COLOR}\",
-#                 \"txt\": \"${TEXT}\",
-#                 \"update\": \"${TIME}\"}"
+echo "API_TOKEN ${API_TOKEN}"
+echo "ORGANIZATION ${ORGANIZATION}"
+echo "REPO ${REPO}"
+echo "APP ${APP}"
+echo "BRANCH ${BRANCH}"
+echo "STATUS ${STATUS}"
+echo "COLOR ${COLOR}"
+echo "TEXT ${TEXT}"
 
 curl -X POST -H "Content-Type: application/json" \
 https://badges.rhems-japan.com/api-update-badge \
@@ -62,3 +32,16 @@ cat << EOF
 "color": "${COLOR}",
 EOF)
 \"update\": \"${TIME}\"}"
+
+
+# curl -X POST -H "Content-Type: application/json" \
+#   https://badges.rhems-japan.com/api-update-badge \
+#   -d "{\"api_token\": \"${API_TOKEN}\",
+#                 \"organization\": \"${ORGANIZATION}\",
+#                 \"repo\": \"${REPO}\",
+#                 \"app\": \"${APP}\",
+#                 \"branch\": \"${BRANCH}\",
+#                 \"status\": \"${STATUS}\",
+#                 \"color\": \"${COLOR}\",
+#                 \"txt\": \"${TEXT}\",
+#                 \"update\": \"${TIME}\"}"
