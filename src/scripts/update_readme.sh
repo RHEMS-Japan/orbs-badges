aa6981d5c699e4e7e52c82e54847be6ecca175f0
ichi-01 <ichikawa@rhems-japan.co.jp>
2022-05-20 16:53:37 +0900

    test

--- src/scripts/update_readme.sh
+++ src/scripts/update_readme.sh
@@ -40,7 +40,11 @@ update_readme () {
       do
         echo -e "\n<< Retry $i >>\n"
         sleep 3
+        git reset --hard HEAD^
         git pull --no-edit
+        edit-readme
+        git add ${FILE_PATH}
+        git commit -m "[skip ci] ${FILE_PATH} Update"
         git push -u origin ${CIRCLE_BRANCH}
         if [ $? -eq 0 ]; then
           break
